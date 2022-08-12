23e6933da60dc549f65eb6834c42a493f6b453e4
Pedro Tonini <pedro.tonini@hotmail.com>
2022-08-12 19:32:22 -0300

    added project files

---
+++ src/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    overrideKey: 'override',
+    workflowTopics: {
+        library: ['library', 'package', 'kong-plugin'],
+        helmRelease: ['helm-release', 'deployment', 'cronjob'],
+        helmChart: ['helm-chart'],
+        docker: ['docker-image'],
+        go: ['vault-plugin'],
+        cloudfront: ['website', 'react-app']
+    },
+    defaults: {
+        MANIFEST_FILE: 'manifest.yaml',
+        RP_MANIFEST_FILE: '.release-please-manifest.json',
+        PUBLISH_CANDIDATE: false,
+        APPROVE_CANDIDATE: false,
+        PROMOTE_CANDIDATE: false,
+        CREATE_STAGING: false,
+        DESTROY_STAGING: false,
+        HOUSEKEEPING: false,
+        REPORTS_FOLDER: "reports",
+        GO_COVERAGE_PROFILE: "coverage.out",
+        GO_TEST_REPORT: "report.out",
+        PYTHON_COVERAGE_REPORT: "*coverage-*.xml",
+        PYTHON_XUNIT_REPORT: "xunit-result-*.xml",
+        JAVASCRIPT_LCOV_INFO: "lcov.info"
+    }
+};
---
+++ src/main.js
@@ -0,0 +1,137 @@
+// Load .env file for local testing
+require('dotenv').config();
+
+const core = require('@actions/core');
+const artifact = require('@actions/artifact');
+const fs = require('fs');
+const yaml = require('yaml');
+const config = require('./config.js');
+const process = require('process');
+const artifactClient = artifact.create();
+
+function getWorkflow(type) {
+    let workflow
+    Object.keys(config.workflowTopics).forEach(k => {
+        if (config.workflowTopics[k].includes(type)) {
+            workflow = k
+        }
+    })
+    return workflow
+}
+
+function aggregateTypes() {
+    let classArray = [];
+    Object.keys(config.workflowTopics).forEach(k => {classArray = classArray.concat(config.workflowTopics[k])})
+    return classArray
+}
+
+function getMetadataFromTopics(label, types, topics, required) {
+    let matches = [];
+    for (const t of topics) {
+        if (Array.isArray(types)) types.includes(t) && matches.push(t);
+        else t in types && matches.push(t);
+    }
+    if (matches.length === 1) {
+        return matches[0];
+    } else if (matches.length === 0) {
+        required && core.setFailed('project missing ' + label + ' topic');
+    }
+    else {
+        core.setFailed('project has multiple ' + label + ' topics [' + matches.join(' ') + ']');
+    }
+}
+
+async function main() {
+
+    const orgConfig = yaml.parse(core.getInput('org_config'));
+    const event = yaml.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf-8'))
+    const projectType = getMetadataFromTopics('type', aggregateTypes(), event.repository.topics, true)
+    const workflow = getWorkflow(projectType)
+
+    const metadata = {...config.defaults}
+    metadata.MANIFEST_FILE = workflow === 'helmChart' ? 'Chart.yaml' : metadata.MANIFEST_FILE
+
+    const manifest = yaml.parse(fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${metadata.MANIFEST_FILE}`, 'utf-8'))
+    const {'.': version } = yaml.parse(fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${metadata.RP_MANIFEST_FILE}`, 'utf-8'))
+    metadata.SKIP_TESTS = manifest['skip_tests'] === true
+    metadata.RELEASE_VERSION = version
+
+    if (process.env.GITHUB_EVENT_NAME === 'pull_request' && ['opened', 'synchronize'].includes(event.action)) {
+        metadata.PUBLISH_CANDIDATE = true
+        metadata.HOUSEKEEPING = true
+    }
+    if (process.env.GITHUB_EVENT_NAME === 'pull_request' && event.action === 'closed') {
+        metadata.APPROVE_CANDIDATE = event.pull_request.merged
+        metadata.HOUSEKEEPING = false
+        metadata.SKIP_TESTS = true
+    }
+    if (process.env.GITHUB_EVENT_NAME === 'push' && event.head_commit.message.includes(`chore(${event.repository.default_branch}): release`)) {
+        metadata.PROMOTE_CANDIDATE = true
+        metadata.HOUSEKEEPING = false
+        metadata.SKIP_TESTS = true
+    }
+
+    if (['helmRelease', 'cloudfront'].includes(workflow)) {
+        metadata.PROJECT_NAME = event.repository.name
+        metadata.PROJECT_APP = getMetadataFromTopics('app', orgConfig.apps, event.repository.topics)
+        metadata.DOCKER_IMAGE_NAME = `${manifest['helm']['values']['image']['registry']}/${manifest['helm']['values']['image']['repository']}`;
+        metadata.REGISTRY_CREDENTIALS_PATH = orgConfig['registry_credentials'][manifest['helm']['values']['image_pull_secrets'][0]]
+        if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
+            const environment = orgConfig['staging_environment']
+            const releaseName = manifest['helm']['release_name']
+            metadata.K8S_ROLE_PATH = orgConfig['environments'][environment]['k8s_role_path']
+            metadata.CREATE_STAGING = !(manifest['skip_staging'] === true)
+            metadata.STAGING_DOMAIN = `${event.number}.${releaseName}.${environment}.${orgConfig['route53']['domain']}`
+            metadata.NAMESPACE = `${releaseName}-${event.number}`
+            if (metadata.CREATE_STAGING && metadata.PUBLISH_CANDIDATE) {
+                metadata.ENVIRONMENT = environment
+                metadata.HELM_EXTRA_ARGUMENTS = '--create-namespace'
+            }
+            if (metadata.CREATE_STAGING && event.action === 'closed' ) {
+                metadata.DESTROY_STAGING = true
+                metadata.AWS_ROLE_PATH = orgConfig['route53']['aws_role_path']
+                metadata.ROUTE53_ZONE_ID = orgConfig['route53']['zone_id']
+            }
+        }
+        if (process.env.GITHUB_EVENT_NAME === 'push') {
+            if (workflow === 'helmRelease') {
+                const environment = manifest['environment']
+                metadata.K8S_ROLE_PATH = orgConfig['environments'][environment]['k8s_role_path']
+                metadata.ENVIRONMENT = environment
+                metadata.NAMESPACE = manifest['helm']['namespace']
+            }
+            if (workflow === 'cloudfront') {
+                metadata.SUBDOMAIN = manifest['subdomain'];
+                metadata.BUCKET = manifest['bucket'];
+                metadata.CUSTOM_TYPES = JSON.stringify(manifest['custom_types'] ?? '[]');
+            }
+        }
+    }
+
+    if (workflow === 'docker') {
+        metadata.DOCKER_IMAGE_NAME = manifest['docker']['image_name'];
+    }
+
+    if (workflow === 'go') {
+        metadata.GO_APP_NAME = manifest['go']['app_name'] ?? event.repository.name
+        metadata.GO_BUILDER_IMAGE = manifest["go"]["builder_image"]
+        metadata.GO_MAIN_FILE = manifest["go"]["main_file"]
+    }
+
+    if (workflow === 'library') {
+        metadata.PROJECT_NAME = event.repository.name
+    }
+
+    if (workflow === 'helmChart') {
+        metadata.CHART_NAME = manifest['name'];
+        metadata.CHART_TYPE = manifest['type'];
+    }
+
+    Object.keys(metadata).forEach(k => {core.setOutput(k.toLowerCase(), metadata[k])})
+    const encodedMetadata = JSON.stringify(metadata, null, 4)
+    core.info(encodedMetadata);
+    fs.writeFileSync('./metadata.json', encodedMetadata);
+    await artifactClient.uploadArtifact('metadata', ['metadata.json'], '.')
+}
+
+main().catch(error => {core.setFailed(error)});
