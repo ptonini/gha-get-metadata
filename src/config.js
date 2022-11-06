cfef6e1acf6c8876616d408826df6545129c3b75
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-06 18:34:12 -0300

    chore: refactoring

--- src/config.js
+++ src/config.js
@@ -1,30 +1,13 @@
 module.exports = {
+    manifestFile: 'manifest.yaml',
+    rpManifestFile: '.release-please-manifest.json',
     overrideKey: 'override',
     workflowTopics: {
         library: ['library', 'package'],
         helmRelease: ['helm-release'],
-        docker: ['docker-image'],
+        docker: ['docker-image', 'admission-controller'],
         go: ['vault-plugin'],
-        cloudfront: ['website', 'react-app'],
+        cloudfront: ['website', 'reactjs-app'],
         luarock: ['kong-plugin']
     },
-    defaults: {
-        MANIFEST_FILE: 'manifest.yaml',
-        RP_MANIFEST_FILE: '.release-please-manifest.json',
-        PUBLISH_CANDIDATE: false,
-        APPROVE_CANDIDATE: false,
-        PROMOTE_CANDIDATE: false,
-        CREATE_STAGING: false,
-        DESTROY_STAGING: false,
-        HOUSEKEEPING: false,
-        REPORTS_FOLDER: "reports",
-        GO_COVERAGE_PROFILE: "coverage.out",
-        GO_TEST_REPORT: "report.out",
-        PYTHON_COVERAGE_REPORT: "*coverage-*.xml",
-        PYTHON_XUNIT_REPORT: "xunit-result-*.xml",
-        JAVASCRIPT_LCOV_INFO: "lcov.info",
-        HELM_SECRET_CHART: 'secret',
-        HELM_SECRET_CHART_VERSION: '^2.0.0',
-        HELM_REPOSITORY: 'https://charts.nodis.com.br'
-    }
 };
--- src/main.js
+++ src/main.js
@@ -21,105 +21,56 @@ function getWorkflow(type) {
 
 function aggregateTypes() {
     let classArray = [];
-    Object.keys(config.workflowTopics).forEach(k => {classArray = classArray.concat(config.workflowTopics[k])})
+    Object.keys(config.workflowTopics).forEach(k => {
+        classArray = classArray.concat(config.workflowTopics[k])
+    })
     return classArray
 }
 
 function getMetadataFromTopics(label, types, topics, required) {
-    let matches = [];
-    for (const t of topics) {
-        if (Array.isArray(types)) types.includes(t) && matches.push(t);
-        else t in types && matches.push(t);
-    }
+    let matches = types.filter(v => topics.includes(v))
     if (matches.length === 1) {
         return matches[0];
     } else if (matches.length === 0) {
         required && core.setFailed('project missing ' + label + ' topic');
-    }
-    else {
+        return ''
+    } else {
         core.setFailed('project has multiple ' + label + ' topics [' + matches.join(' ') + ']');
     }
 }
 
+
 async function main() {
 
-    const orgConfig = yaml.parse(core.getInput('org_config'));
+    const vaultK8sRolePaths = yaml.parse(core.getInput('vault_k8s_role_paths'));
     const event = yaml.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf-8'))
     const projectType = getMetadataFromTopics('type', aggregateTypes(), event.repository.topics, true)
+    const workspace = process.env.GITHUB_WORKSPACE
     const workflow = getWorkflow(projectType)
-    const metadata = {...config.defaults}
+    const metadata = {}
 
     let manifest = {}
-    if (fs.existsSync(metadata.MANIFEST_FILE)) {
-        manifest = yaml.parse(fs.readFileSync(metadata.MANIFEST_FILE, 'utf-8'))
+    if (fs.existsSync(`${workspace}/${config.manifestFile}`)) {
+        manifest = yaml.parse(fs.readFileSync(`${workspace}/${config.manifestFile}`, 'utf-8'))
     }
 
     let version = '0.0.0'
-    if (fs.existsSync(metadata.RP_MANIFEST_FILE)) {
-        version = yaml.parse(fs.readFileSync(metadata.RP_MANIFEST_FILE, 'utf-8'))['.']
+    if (fs.existsSync(`${workspace}/${config.rpManifestFile}`)) {
+        version = yaml.parse(fs.readFileSync(`${workspace}/${config.rpManifestFile}`, 'utf-8'))['.']
+    }
+    core.setOutput('RELEASE_VERSION', version)
+
+    if (workflow === 'helmRelease' || workflow === 'cloudfront') {
+        core.setOutput('DOCKER_REPOSITORY', `${manifest['helm']['values']['image']['registry']}/${manifest['helm']['values']['image']['repository']}`)
+        core.setOutput('VAULT_K8S_ROLE_PATH',vaultK8sRolePaths[manifest['environment']])
     }
 
-    metadata.SKIP_TESTS = manifest['skip_tests'] === true
-    metadata.RELEASE_VERSION = version
-
-    if (process.env.GITHUB_EVENT_NAME === 'pull_request' && ['opened', 'synchronize', 'reopened'].includes(event.action)) {
-        metadata.PUBLISH_CANDIDATE = true
-        metadata.HOUSEKEEPING = !(manifest['skip_housekeeping'] === true)
-    }
-
-    if (process.env.GITHUB_EVENT_NAME === 'pull_request' && event.action === 'closed') {
-        metadata.APPROVE_CANDIDATE = event.pull_request.merged
-        metadata.HOUSEKEEPING = false
-        metadata.SKIP_TESTS = true
-    }
-
-    if (process.env.GITHUB_EVENT_NAME === 'push' && event.head_commit.message.includes(`chore(${event.repository.default_branch}): release`)) {
-        metadata.PROMOTE_CANDIDATE = true
-        metadata.HOUSEKEEPING = false
-        metadata.SKIP_TESTS = true
-    }
-
-    if (['helmRelease', 'cloudfront'].includes(workflow)) {
-        const imagePullSecret = manifest['helm']['values']['imagePullSecrets'][0]
-        metadata.APP_GROUP = getMetadataFromTopics('app', orgConfig.apps, event.repository.topics)
-        metadata.DOCKER_REPOSITORY = `${manifest['helm']['values']['image']['registry']}/${manifest['helm']['values']['image']['repository']}`
-        metadata.IMAGE_PULL_SECRET = imagePullSecret
-        metadata.VAULT_REGISTRY_CREDENTIALS_PATH = orgConfig['registry_credentials'][imagePullSecret]
-        metadata.VAULT_GITHUB_TOKEN_PATH = orgConfig['github_token_path']
-        metadata.INGRESS_CONFIG_REPOSITORY = orgConfig['ingress_config_repository']
-        metadata.PRD_NAMESPACE = manifest['helm']['namespace']
-        metadata.PRD_ENVIRONMENT = manifest['environment']
-        metadata.INGRESS_PATH = `${manifest['helm']['namespace']}/${manifest['environment']}`
-        if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
-            const environment = orgConfig['staging_environment']
-            const releaseName = manifest['helm']['release_name']
-            metadata.VAULT_K8S_ROLE_PATH = orgConfig['environments'][environment]['k8s_role_path']
-            metadata.CREATE_STAGING = !(manifest['skip_staging'] === true)
-            metadata.STAGING_DOMAIN = `${event.number}.${releaseName}.${environment}.${orgConfig['route53']['domain']}`
-            metadata.NAMESPACE = `${releaseName}-${event.number}`
-            if (metadata.CREATE_STAGING && metadata.PUBLISH_CANDIDATE) {
-                metadata.ENVIRONMENT = environment
-                metadata.HELM_EXTRA_ARGUMENTS = '--create-namespace'
-            }
-            if (metadata.CREATE_STAGING && event.action === 'closed' ) {
-                metadata.DESTROY_STAGING = true
-            }
-        }
-        if (['workflow_dispatch', 'push'].includes(process.env.GITHUB_EVENT_NAME)) {
-            if (workflow === 'helmRelease') {
-                const environment = manifest['environment']
-                metadata.VAULT_K8S_ROLE_PATH = orgConfig['environments'][environment]['k8s_role_path']
-                metadata.ENVIRONMENT = environment
-                metadata.NAMESPACE = manifest['helm']['namespace']
-            }
-            if (workflow === 'cloudfront') {
-                metadata.ARTIFACT_NAME = `${event.repository.name}-${version}.tar.gz`
-                metadata.VAULT_AWS_ROLE_PATH = manifest['cloudfront']['aws_role_path']
-                metadata.DISTRIBUTION_ID = manifest['cloudfront']['distribution_id'];
-                metadata.BUCKET = manifest['cloudfront']['bucket'];
-                metadata.CUSTOM_TYPES = JSON.stringify(manifest['cloudfront']['custom_types'] ?? '[]');
-            }
-        }
+    if (workflow === 'cloudfront') {
+        metadata.ARTIFACT_NAME = `${event.repository.name}-${version}.tar.gz`
+        metadata.VAULT_AWS_ROLE_PATH = manifest['cloudfront']['aws_role_path']
+        metadata.DISTRIBUTION_ID = manifest['cloudfront']['distribution_id'];
+        metadata.BUCKET = manifest['cloudfront']['bucket'];
+        metadata.CUSTOM_TYPES = JSON.stringify(manifest['cloudfront']['custom_types'] ?? '[]');
     }
 
     if (workflow === 'docker') {
@@ -130,11 +81,11 @@ async function main() {
         metadata.GO_APP_NAME = event.repository.name
         metadata.GO_MAIN_FILE = "main.go"
         metadata.GO_BUILDER_IMAGE = manifest["go"]["builder_image"]
-        }
+    }
 
     if (workflow === 'luarock') {
         metadata.PACKAGE_NAME = event.repository.name
-        metadata.ROCK_PREFIX = `${event.repository.name }-${version}`
+        metadata.ROCK_PREFIX = `${event.repository.name}-${version}`
         metadata.ROCK_FILE = `${metadata.ROCK_PREFIX}-0.rockspec`
     }
 
@@ -143,7 +94,7 @@ async function main() {
         core.info(`${k}=${metadata[k]}`)
     })
 
-    fs.writeFileSync('./metadata.json',  JSON.stringify(metadata, null, 4));
+    fs.writeFileSync('./metadata.json', JSON.stringify(metadata, null, 4));
     await artifactClient.uploadArtifact('metadata', ['metadata.json'], '.')
 
     fs.writeFileSync('./event.json', JSON.stringify(event, null, 4));
@@ -151,4 +102,6 @@ async function main() {
 
 }
 
-main().catch(error => {core.setFailed(error)});
+main().catch(error => {
+    core.setFailed(error)
+});
